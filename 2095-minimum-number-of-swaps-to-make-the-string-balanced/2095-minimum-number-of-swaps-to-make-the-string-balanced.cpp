c38904b4d9a35618cd4ec1cba26d1e42a65dc048
Priyanshu Anand <priyanshuanandd@gmail.com>
2024-10-09 23:42:27 +0530

    Time: 149 ms (9.12%), Space: 37.1 MB (9.8%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        int n = s.size();
-        int balance = 0, swaps = 0, j = n - 1;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '[')
-                balance++;
-            else
-                balance--;
-
-            if (balance < 0) {
-                while (i < j && s[j] != '[')
-                    j--;
-                swap(s[i], s[j]);
-                swaps++;
-                balance = 1;
+        stack<char> stk;
+        if (s.size() % 2) {
+            return -1;
+        }
+        for (auto i : s) {
+            if (i == ']' and (stk.empty() or stk.top() == ']')) {
+                stk.push(']');
+            } else if (i == ']') {
+                stk.pop();
+            } else if (i == '[') {
+                stk.push(i);
             }
         }
-        return swaps;
-    }
+        int open = 0;
+        int closed = 0;
+
+        while (!stk.empty()) {
+            if (stk.top() == ']') {
+                open++;
+            } else {
+                closed++;
+            }
+            stk.pop();
+        }
+
+        return ((open+1)/2);
+    }    
 };
